a1f31b26d8df74bb1850a7b59d38df7a65e30c97
LokeshKvms <loki.kvms@gmail.com>
2025-06-12 17:14:51 +0530

    opt migrations

--- database/migrations/2025_06_11_111439_create_links_table.php
+++ database/migrations/2025_06_11_111439_create_links_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
             $table->string('title');
             $table->string('url');
+            $table->string('image_url')->nullable();
             $table->timestamps();
         });
     }
